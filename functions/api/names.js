716b0662f8a020e77fefbac55e66b7dd513f23ee
plutotechx <it.sp.plutotech@gmail.com>
2026-03-10 13:54:06 +0700

    Update names.js

--- functions/api/names.js
+++ functions/api/names.js
@@ -1,16 +1,12 @@
+import { json, withAction } from "./_util.js";
+
 export async function onRequestGet({ env }) {
-  const url = new URL(env.GAS_URL);
-  url.searchParams.set("action", "names");
-  url.searchParams.set("secret", env.SECRET);
-
-  const res = await fetch(url.toString(), { method: "GET" });
-  const out = await res.json().catch(() => ({}));
-  return json(out, res.ok ? 200 : 500);
-}
-
-function json(obj, status = 200) {
-  return new Response(JSON.stringify(obj), {
-    status,
-    headers: { "Content-Type": "application/json; charset=utf-8" },
-  });
+  try {
+    const url = withAction(env.GAS_URL, "names", env.SECRET);
+    const res = await fetch(url, { method: "GET" });
+    const out = await res.json().catch(() => ({}));
+    return json(out, res.ok ? 200 : 500);
+  } catch (err) {
+    return json({ ok: false, error: String(err) }, 500);
+  }
 }
