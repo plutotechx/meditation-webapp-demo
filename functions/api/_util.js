5aa6f66b549ed7d3422cdae344cdce11c341f4ad
plutotechx <it.sp.plutotech@gmail.com>
2026-03-10 13:52:57 +0700

    Update _util.js

--- functions/api/_util.js
+++ functions/api/_util.js
@@ -6,7 +6,6 @@ export function getEnv(ctx, key) {
 
 export function withAction(gasUrl, action, secret) {
   const u = new URL(gasUrl);
-  // ล้าง query เก่าทั้งหมด เผื่อมี action/secret ติดมา
   u.search = "";
   u.searchParams.set("action", action);
   u.searchParams.set("secret", secret);
@@ -16,6 +15,10 @@ export function withAction(gasUrl, action, secret) {
 export function json(data, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
-    headers: { "Content-Type": "application/json; charset=utf-8" }
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Cache-Control": "no-store",
+      "Access-Control-Allow-Origin": "*"
+    }
   });
 }
